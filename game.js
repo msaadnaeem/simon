a36612e6d111a47d0ea15057352c17536c7dd165
msaadnaeem <110536997+msaadnaeem@users.noreply.github.com>
2022-08-24 16:03:53 +0500

    Update game.js

--- game.js
+++ game.js
@@ -1,3 +1,17 @@
+$('body').addClass('start')
+$('#red').css('opacity', 0)
+$('#yellow').css('opacity', 0)
+$('#green').css('opacity', 0)
+$('#blue').css('opacity', 0)
+$("#level-title").text("Click here \n to start");
+$("#score").text("How to play");
+$("#inst1").text("A color will flash. Repeat the sequence by pressing the same color.");
+$("#inst2").text("The game will continue. After each light sequence that you repeat successfully");
+$("#inst3").text("A light will be added to the end.");
+$("#inst4").text("If you don’t complete a sequence successfully, you will fail.");
+$("#inst5").text("Try to beat your high score!");
+
+
 function random() {
   return Math.floor(Math.random() * 4);
 }
@@ -9,17 +23,19 @@ var level = 1;
 var click = 1;
 var check = 0;
 
+
+
 function genTile() {
   $("#level-title").text("Level " + level);
   const id = colors[random()];
   pattern.push(id);
-//  const audio = new Audio("sounds/" + id + ".mp3");
+  //  const audio = new Audio("sounds/" + id + ".mp3");
   setTimeout(function () {
     $("#" + id).addClass("pressed");
     setTimeout(function () {
       $("#" + id).removeClass("pressed");
     }, 250);
- //   audio.play();
+    //   audio.play();
   }, 350);
 }
 
@@ -27,6 +43,18 @@ $("#level-title").click(function () {
   if (!started) {
     $("#score").text("");
     $("#high-score").text("");
+    $('body').removeClass('start')
+    $('#red').css('opacity', 100)
+    $('#yellow').css('opacity', 100)
+    $('#green').css('opacity', 100)
+    $('#blue').css('opacity', 100)
+    $("#inst1").text("");
+    $("#inst2").text("");
+    $("#inst3").text("");
+    $("#inst4").text("");
+    $("#inst5").text("");
+
+
     genTile();
     started = true;
   }
@@ -36,12 +64,12 @@ $("div[type='button']").click(function () {
   const id = $(this).attr("id");
 
   if (started) {
-   // const audio = new Audio("sounds/" + id + ".mp3");
+    // const audio = new Audio("sounds/" + id + ".mp3");
     $("#" + id).addClass("pressed");
     setTimeout(function () {
       $("#" + id).removeClass("pressed");
     }, 150);
-   // audio.play();
+    // audio.play();
   }
   if (click <= level && started) {
     if (pattern[check] === id) {
